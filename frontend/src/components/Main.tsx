529d4ae222fb33d60911d063f2663d1f1a3fd25e
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2024-06-10 13:08:55 -0300

    Separando responsabilidades

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import ApiService from '../services/ApiService';
+import React from 'react';
+import Home from './Home';
 import '../styles/main.css';
 
-const apiService = new ApiService('http://localhost:3000/');
-
-interface Produto {
-  idproduto: number;
-  nome: string;
-  descricao: string;
-  preco: string;
-  quantidade: string;
-}
-
 const Main: React.FC = () => {
-  const [produtos, setClientes] = useState<Produto[]>([]);
-
-  useEffect(() => {
-    const fetchClientes = async () => {
-      try {
-        const data = await apiService.get('/produto');
-        setClientes(data);
-      } catch (error) {
-        console.error('Erro ao buscar clientes:', error);
-      }
-    };
-
-    fetchClientes();
-  }, []);
-
   return (
     <main>
       <section id='home'>
-        <h1>Site em construção</h1>
-        <ul>
-        {produtos.map(cliente => (
-          <li key={cliente.idproduto}>
-            {cliente.nome} - {cliente.preco}
-          </li>
-        ))}
-        </ul>        
+        {Home()}
       </section>
       <section id='about'>
         <h1>Site em construção</h1>
