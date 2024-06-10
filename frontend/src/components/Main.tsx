ea87710004987c0db29be57d90dcbb4a0e38939c
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2024-06-10 11:44:45 -0300

    Upgrade frontend - gets from bd

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ApiService from '../services/ApiService';
 import '../styles/main.css';
 
+const apiService = new ApiService('http://localhost:3000/');
+
+interface Produto {
+  idproduto: number;
+  nome: string;
+  descricao: string;
+  preco: string;
+  quantidade: string;
+}
+
 const Main: React.FC = () => {
+  const [produtos, setClientes] = useState<Produto[]>([]);
+
+  useEffect(() => {
+    const fetchClientes = async () => {
+      try {
+        const data = await apiService.get('/produto');
+        setClientes(data);
+      } catch (error) {
+        console.error('Erro ao buscar clientes:', error);
+      }
+    };
+
+    fetchClientes();
+  }, []);
+
   return (
     <main>
       <section id='home'>
         <h1>Site em construção</h1>
-        <h1>Home</h1>
+        <ul>
+        {produtos.map(cliente => (
+          <li key={cliente.idproduto}>
+            {cliente.nome} - {cliente.preco}
+          </li>
+        ))}
+        </ul>        
       </section>
       <section id='about'>
         <h1>Site em construção</h1>
