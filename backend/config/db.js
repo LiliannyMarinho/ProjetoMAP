99e2dfeacea6e36509875337af62433b3b16c599
Everaldo Martins <46751872+Everaldo-Martins@users.noreply.github.com>
2024-06-09 22:34:30 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'projeto_map',
-    password: 'Thmpv07',
+    database: '',
+    password: '',
     port: 5432,
 });
 
