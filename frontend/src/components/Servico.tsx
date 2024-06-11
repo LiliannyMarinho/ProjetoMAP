4e1f7dd18a672bdc7f45764086a89622bd79bdaf
LiliannyMarinho <liliannymarinho@gmail.com>
2024-06-11 14:48:45 -0300

    Atualização

--- frontend/src/components/Servico.tsx
+++ frontend/src/components/Servico.tsx
@@ -7,27 +7,23 @@ import shampoocondicionador from '../assets/shampoocondicionador.webp';
 const Servico: React.FC = () => {
     return (
         <>
-            <main>
-                <section id="s1" className="servicos">
-                    <div className="articles-container">
-                        <article id="a1" className="servico">
-                            <img src={estetica} alt="estetica" className="info-image" />
-                            <h3>Banho e Tosa</h3>
-                            <p>Deixe seu pet limpo e cheiroso com nossos serviços de banho e tosa.</p>
-                        </article>
-                        <article id="a2" className="servico">
-                            <img src={vet} alt="vet" className="info-image" />
-                            <h3>Veterinário</h3>
-                            <p>Cuidados veterinários de qualidade para a saúde do seu pet.</p>
-                        </article>
-                        <article id="a3" className="servico">
-                            <img src={shampoocondicionador} alt="shampoo" className="info-image" />
-                            <h3>Produtos</h3>
-                            <p>Garanta a melhor qualidade em produtos para seu pet.</p>
-                        </article>
-                    </div>
-                </section>
-            </main>
+            <div className="articles-container">
+                <article id="a1" className="servico">
+                    <img src={estetica} alt="estetica" className="info-image" />
+                    <h3>Banho e Tosa</h3>
+                    <p>Deixe seu pet limpo e cheiroso com nossos serviços de banho e tosa.</p>
+                </article>
+                <article id="a2" className="servico">
+                    <img src={vet} alt="vet" className="info-image" />
+                    <h3>Veterinário</h3>
+                    <p>Cuidados veterinários de qualidade para a saúde do seu pet.</p>
+                </article>
+                <article id="a3" className="servico">
+                    <img src={shampoocondicionador} alt="shampoo" className="info-image" />
+                    <h3>Produtos</h3>
+                    <p>Garanta a melhor qualidade em produtos para seu pet.</p>
+                </article>
+            </div>
         </>
     );
 };
